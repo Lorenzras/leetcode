20194ead8835e8ff82e4d19365b7f6ff88e50790
Lorenz Ras <lenzras@gmail.com>
2024-02-07 16:46:18 +0900

    Time: 84 ms (32.08%) | Memory: 55.9 MB (15.09%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.ts
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.ts
@@ -2,15 +2,22 @@
 function maximumNumberOfStringPairs(words: string[]): number {
     const wlen = words.length;
     let pairs = 0;
+    
 
     for(let i = 0; i < wlen; i++) {
+        if(!words[i]) continue;
+
         for(let j = 0; j < wlen; j++) {
             if(i == j) continue;
+            if(!words[j]) continue;
+
             if(words[i] === words[j].split('').reverse().join('')){
+                words[i] = undefined;
+                words[j] = undefined;
                 pairs++;
             }
         }
     }
 
-    return pairs / 2;
+    return pairs;
 };

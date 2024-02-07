09dc5b615eba7127c40aee47ab42870b6fdaf469
Lorenz Ras <lenzras@gmail.com>
2024-02-07 17:41:23 +0900

    Time: 121 ms (43.06%) | Memory: 64.4 MB (10.55%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.ts
@@ -0,0 +1,15 @@
+function groupAnagrams(strs: string[]): string[][] {
+
+    const resultObj = strs.reduce((acc, cur) => {
+        const normalized = Array.from(cur).sort().join('');
+        if(!acc[normalized]) {
+            acc[normalized] = [cur]
+        } else {
+            acc[normalized].push(cur);
+        }
+
+        return acc; 
+    }, {} as  Record<string, string[]>)
+
+    return Object.values(resultObj);  
+};

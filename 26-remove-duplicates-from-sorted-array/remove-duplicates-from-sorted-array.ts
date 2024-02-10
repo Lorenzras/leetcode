c2391b58bdc54abb50f8093fabe2e12a2b1d7908
Lorenz Ras <lenzras@gmail.com>
2024-02-10 16:31:52 +0900

    Time: 56 ms (95.47%) | Memory: 53.1 MB (19.64%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,6 +1,5 @@
 function removeDuplicates(nums: number[]): number {
     const sortedNums = Array.from(new Set(nums)).sort((a, b) => a - b);
-    //const expectNums = [1, 2];
     nums.forEach((item, idx) => {
         nums[idx] = sortedNums?.[idx]; 
     })

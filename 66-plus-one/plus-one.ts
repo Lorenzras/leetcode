4ad508defa61cb0ab7a72685bd38e5c49c9fe6dd
Lorenz Ras <lenzras@gmail.com>
2024-02-11 12:00:44 +0900

    Time: 59 ms (49.21%) | Memory: 51.1 MB (32.95%) - LeetSync

--- 66-plus-one/plus-one.ts
+++ 66-plus-one/plus-one.ts
@@ -1,5 +1,4 @@
 function plusOne(digits: number[]): number[] {
     const combined = BigInt(digits.join(''));
-    console.log(combined);
     return String(combined + 1n).split('').map((d) => +d);
 };

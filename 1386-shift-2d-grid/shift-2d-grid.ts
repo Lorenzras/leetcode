2a00c73b71f7220788090a16432e6f07be7b008a
Lorenz Ras <lenzras@gmail.com>
2024-02-11 18:16:25 +0900

    Time: 84 ms (71.43%) | Memory: 57.8 MB (14.29%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.ts
+++ 1386-shift-2d-grid/shift-2d-grid.ts
@@ -1,11 +1,10 @@
 function shiftGrid(grid: number[][], k: number): number[][] {
-    const flatArr = grid.flat();
+    let flatArr = grid.flat();
     const width = grid[0].length;
 
     for(let i = 0; i < k; i++){
         flatArr.unshift(flatArr.pop());
     }
-
 
     for(let i = 0; i < grid.length; i++){
         const seg = i * width;

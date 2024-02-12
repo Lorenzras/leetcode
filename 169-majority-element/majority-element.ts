d8e0ce452ff75f41cc30e2c4959e58f8a377cdb0
Lorenz Ras <lenzras@gmail.com>
2024-02-12 15:45:37 +0900

    Time: 77 ms (16.25%) | Memory: 54.3 MB (7.30%) - LeetSync

---
+++ 169-majority-element/majority-element.ts
@@ -0,0 +1,21 @@
+function majorityElement(nums: number[]): number {
+    const mapNums = new Map();
+    let majorityEl = null; 
+    let maxCount = 0;
+    
+    for(let i = 0; i < nums.length; i++){
+        
+        mapNums.set(nums[i], (mapNums.get(nums[i]) ?? 0) + 1);
+        
+        if(mapNums.get(nums[i]) > maxCount ){
+            maxCount = mapNums.get(nums[i]);
+            majorityEl = nums[i];
+        }
+
+        //console.log(nums[i], '----', mapNums.get(nums[i]),  maxCount, majorityEl);
+    }
+
+
+    return majorityEl;
+
+};

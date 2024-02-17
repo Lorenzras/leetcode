024fa79f74faf697a0ec8a06a324ed4a5678ec1a
Lorenz Ras <lenzras@gmail.com>
2024-02-17 12:19:34 +0900

    Time: 60 ms (44.74%) | Memory: 51 MB (21.05%) - LeetSync

---
+++ 1769-get-maximum-in-generated-array/get-maximum-in-generated-array.ts
@@ -0,0 +1,12 @@
+function getMaximumGenerated(n: number): number {
+    if(n === 0){
+        return 0
+    }
+    let nums = [0, 1]
+    for (let i = 1; i < n/2; i++) {
+        nums[i*2]=nums[i]
+        nums[(2*i)+1]=nums[i+1]+nums[i]
+    }
+
+    return Math.max(...nums)
+};

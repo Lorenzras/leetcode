2085244c54b4e0ddf87403cd964502203522d111
Lorenz Ras <lenzras@gmail.com>
2024-02-17 13:44:28 +0900

    Time: 77 ms (43.66%) | Memory: 55.5 MB (9.86%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.ts
@@ -0,0 +1,15 @@
+function findMissingAndRepeatedValues(grid: number[][]): number[] {
+    const n = grid.length;
+    const ans = [0, 0], set = new Set();
+    const needSum = (n**2)*(n**2+1)/2;
+    let sum = 0;
+    for (const row of grid) {
+        for (const num of row) {
+            if (set.has(num)) ans[0] = num;
+            set.add(num);
+            sum += num;
+        }
+    }
+    ans[1] = needSum + ans[0] - sum;
+    return ans; 
+};

fd57d179d7ec5836723e297d02b201917486aa37
Lorenz Ras <lenzras@gmail.com>
2024-02-17 10:50:43 +0900

    Time: 1000 ms (11.11%) | Memory: 64.4 MB (44.44%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.ts
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.ts
@@ -1,33 +1,59 @@
 function furthestBuilding(heights: number[], bricks: number, ladders: number): number {
-    //DESC
-    const maxDiffs: number[] = Array(ladders).fill(0);
-    function findInfInd(target: number): number {
-        if (maxDiffs[maxDiffs.length - 1] >= target) return maxDiffs.length;
-        let start = 0;
-        let end = maxDiffs.length - 1;
-        while (start < end) {
-            const mid = (start + end) >> 1;
-            if (target >= maxDiffs[mid]) {
-                end = mid;
-            } else {
-                start = mid + 1;
-            }
+    let max = []
+    let [tempBricks, tempLadders] = [bricks, ladders]
+
+    function sortedIndex(array, value) {
+        var low = 0,
+        high = array.length;
+
+        while (low < high) {
+            var mid = (low + high) >>> 1;
+            if (array[mid] < value) low = mid + 1;
+            else high = mid;
         }
-        return start;
+        return low;
     }
 
-    let diffSum = 0;
-    for (let i = 1; i < heights.length; i++) {
-        const diff = Math.max(0, heights[i] - heights[i - 1]);
-        const infInd = findInfInd(diff);
-        if (infInd !== maxDiffs.length) {
-            maxDiffs.splice(infInd, 0, diff);
-            diffSum += maxDiffs.pop()!;
-        } else {
-            diffSum += diff;
-        }
-        if (diffSum > bricks) return i - 1;
+    function addDiff(diff) {
+        if(!tempLadders) return
+
+        let index = sortedIndex(max, diff)
+        max.splice(index, 0, diff);
+
+        if(tempLadders <= max.length - 1) max.shift()  
     }
     
-    return heights.length - 1;
+    function useLadder(temp) {
+        if(!tempLadders) return temp
+
+        let diff = max.pop()
+        tempLadders--
+        return temp + diff
+    }
+
+    for(let i = 0; i < heights.length; i++) {
+        let curr = heights[i]
+        let next = heights[i+1]
+        
+		// Return if you reach the end
+        if(!next) return i
+        
+		// Jump if smaller building
+        if(curr >= next) continue
+        
+		// Calculate height differece
+        let diff = next - curr
+		
+		// Add the difference in sorted array if max size equal to ladder
+        addDiff(diff)
+        
+		// Use the ladder if required to free up biggest bricks space
+        if(tempBricks < diff) tempBricks = useLadder(tempBricks)
+
+		// Subtract bricks
+        tempBricks -= diff
+
+		// Return index. if you run out of bricks
+        if(tempBricks < 0) return i
+    }
 };

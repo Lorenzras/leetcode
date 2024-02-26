fe6cef56455ea58f0ee2279b53aecef3073c911c
Lorenz Ras <lenzras@gmail.com>
2024-02-26 16:08:03 +0900

    Time: 12 ms (44.15%) | Memory: 7.1 MB (46.49%) - LeetSync

---
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.go
@@ -0,0 +1,23 @@
+func reverseWords(s string) string {
+    words := strings.Split(s, " ")
+    reversed := []rune(s)
+
+    idx := 0
+
+    for _, ws := range words {
+        wordRunes := []rune(ws)
+        for i := len(ws) - 1; i >= 0; i-- {
+            reversed[idx] = wordRunes[i]
+            idx++
+        }
+
+        if idx < len(s)  {
+            reversed[idx] = ' '
+            idx++
+        }
+
+    }
+    
+
+    return string(reversed);
+}

32acc9fa7a03e1faf1b3809dc4704e9d2e3edc42
Lorenz Ras <lenzras@gmail.com>
2024-03-08 16:56:41 +0900

    Time: 611 ms (22.63%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,13 +1,8 @@
 # Write your MySQL query statement below
-SELECT 
-    CASE
-        WHEN COUNT(DISTINCT salary) > 1 THEN (
-            SELECT DISTINCT salary
-            FROM Employee
-            ORDER BY salary DESC
-            LIMIT 1 OFFSET 1
-        )
-        ELSE NULL
-    END AS SecondHighestSalary 
-FROM Employee
+SELECT(SELECT DISTINCT
+    Salary 
+FROM
+    Employee
+ORDER BY Salary DESC
+LIMIT 1 OFFSET 1)AS SecondHighestSalary;
 

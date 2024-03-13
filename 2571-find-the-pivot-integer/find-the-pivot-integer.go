ac96c78099223786f7a2b7f376c4983112a8ba99
Lorenz Ras <lenzras@gmail.com>
2024-03-13 13:42:13 +0900

    Time: 0 ms (100.00%) | Memory: 2 MB (25.00%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.go
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.go
@@ -1,9 +1,7 @@
 func pivotInteger(n int) int {
     for i := n / 2; i <= n; i++ {
-        leftSum := ((i) * (1 + i)) / 2
-        rightSum := ((n - i + 1) * (i + n)) / 2
 
-        if leftSum == rightSum {
+        if ((i) * (1 + i)) / 2 == ((n - i + 1) * (i + n)) / 2 {
             return i
         } 
 
